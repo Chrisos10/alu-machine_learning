182de29c62b20f321a161a4ff10e17e00e230027
Chrisos10 <j.dufitumuk@alustudent.com>
2024-09-06 09:04:39 +0200

    pycodestyle

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+# Plotting x and y with a redline, and limitting x to range 10
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
-x = np.arange(0,11)
+x = np.arange(0, 11)
 
 plt.plot(x, y, "r-")
 plt.xlim(0, 10)
