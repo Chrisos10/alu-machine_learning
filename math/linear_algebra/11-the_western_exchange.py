5aef8a0cfd4e0ef5e50081e7bb342a089b9003a6
Chrisos10 <j.dufitumuk@alustudent.com>
2024-09-13 14:49:57 +0200

    transposing

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+    A function that transposes matrix:
+'''
+
+
+def np_transpose(matrix):
+    '''
+        transposing the matrix:
+    '''
+    return matrix.transpose()
