8e156413e4cea01423ed9626690cd128419b1808
Chrisos10 <j.dufitumuk@gmail.com>
2024-10-09 05:30:29 -0700

    initialized normal

---
+++ math/probability/normal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Create a class Normal that represents a normal distribution
+"""
+
+
+class Normal:
+    """class Normal"""
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Constructor"""
+
+        self.E = 2.7182818285
+        self.PI = 3.1415926536
+
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            else:
+                self.stddev = float(stddev)
+            self.mean = float(mean)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.mean = (sum(data) / len(data))
+            self.stddev = (sum([(x - self.mean) ** 2 for x in data])
+                           / len(data)) ** 0.5
