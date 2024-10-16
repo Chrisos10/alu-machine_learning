5afb2d9c93e357867ddaf0ceb1d825e105d7d6a3
Chrisos10 <j.dufitumuk@alustudent.com>
2024-10-16 14:40:50 +0200

    multivariate normal distribution

---
+++ math/multivariate_prob/multinormal.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+    class MultiNormal that represents
+    a Multivariate Normal distribution:
+"""
+
+
+import numpy as np
+
+
+class MultiNormal:
+    """
+    class MultiNormal that represents
+    a Multivariate Normal distribution:
+    """
+
+    def __init__(self, data):
+        """
+        Args:
+            data is a numpy.ndarray of shape (n, d)
+            containing the data set
+        """
+        if type(data) is not np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        d, n = data.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+
+        self.data = data
+        mean = np.mean(data, axis=1, keepdims=True)
+        self.mean = mean
+        cov = np.matmul(data - mean, data.T - mean.T) / (n - 1)
+        self.cov = cov
+        
